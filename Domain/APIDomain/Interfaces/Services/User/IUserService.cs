32f517e8f7c5f4ed0ddf7f5a9005377bef623a2f
Federico <freddyhewlett@gmail.com>
2022-02-04 09:02:34 -0300

    userservice com interface

---
+++ Domain/APIDomain/Interfaces/Services/User/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace APIDomain.Interfaces.Services.User
+{
+    public interface IUserService
+    {
+        Task<Entities.User.User> Get(Guid id);
+        Task<IEnumerable<Entities.User.User>> GetAll();
+        Task<Entities.User.User> Post(Entities.User.User user);
+        Task<Entities.User.User> Put(Entities.User.User user);
+        Task<bool> Delete(Guid id);
+    }
+}
---
+++ Domain/APIDomain/Services/UserService.cs
@@ -0,0 +1,44 @@
+﻿using APIDomain.Entities.User;
+using APIDomain.Interfaces.Repositories;
+using APIDomain.Interfaces.Services.User;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace APIDomain.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IBaseRepository<User> _repository;
+
+        public UserService(IBaseRepository<User> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            return await _repository.DeleteAsync(id);
+        }
+
+        public async Task<User> Get(Guid id)
+        {
+            return await _repository.SelectAsync(id);
+        }
+
+        public async Task<IEnumerable<User>> GetAll()
+        {
+            return await _repository.SelectAllAsync();
+        }
+
+        public async Task<User> Post(User user)
+        {
+            return await _repository.InsertAsync(user);
+        }
+
+        public async Task<User> Put(User user)
+        {
+            return await _repository.UpdateAsync(user);
+        }
+    }
+}
