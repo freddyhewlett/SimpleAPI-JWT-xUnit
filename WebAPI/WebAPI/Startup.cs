2fd947bf6919146d650f44c5046e22683ee897e2
Federico <freddyhewlett@gmail.com>
2022-02-04 17:19:28 -0300

    refatoração + implementação swagger

--- WebAPI/WebAPI/Startup.cs
+++ WebAPI/WebAPI/Startup.cs
@@ -30,20 +30,8 @@ namespace WebAPI
                 c.SwaggerDoc("v1", new OpenApiInfo
                 {
                     Version = "v1",
-                    Title = "Curso de API com AspNetCore 3.1 - Na Prática",
-                    Description = "Arquitetura DDD",
-                    TermsOfService = new Uri("http://www.mfrinfo.com.br"),
-                    Contact = new OpenApiContact
-                    {
-                        Name = "Marcos Fabricio Rosa",
-                        Email = "mfr@mail.com",
-                        Url = new Uri("http://www.mfrinfo.com.br")
-                    },
-                    License = new OpenApiLicense
-                    {
-                        Name = "Termo de Licença de Uso",
-                        Url = new Uri("http://www.mfrinfo.com.br")
-                    }
+                    Title = "Projeto API com AspNetCore 3.1",
+                    Description = "Arquitetura DDD"
                 });
             });
 
@@ -61,6 +49,13 @@ namespace WebAPI
             }
 
             app.UseHttpsRedirection();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API com AspNetCore 3.1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseRouting();
 
